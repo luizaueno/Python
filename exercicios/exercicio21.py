4c011cf4fa648880c49c100ad776c0b9e4c5d5fc
luiza <161471348+luizaueno@users.noreply.github.com>
2025-12-17 20:10:50 -0300

    exercicio 21 inicio

--- exercicios/exercicio21.py
+++ exercicios/exercicio21.py
@@ -1,2 +1,8 @@
 # Leia o nome completo de uma pessoa, mostre todas as minusculas, maiusculas, quantas letras sem espaço e quantas tem o primeiro nome
 
+nome = input('Digite seu nome completo: ')
+print(nome[1:6], nome[7:] )
+print(nome[0], nome[6])
+
+# Luiza Akemi
+
