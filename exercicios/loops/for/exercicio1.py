12debcb1f8031504643079741dec9324494485a2
luiza <161471348+luizaueno@users.noreply.github.com>
2025-12-26 14:57:01 -0300

    exercicios de loops inicio

--- exercicios/loops/for/exercicio1.py
+++ exercicios/loops/for/exercicio1.py
@@ -1,1 +1,8 @@
 # Mostre na tela uma contagem regressiva e de uma pausa de 1 segundo entre cada um
+
+import time
+print('Contagem regressiva! ')
+for c in range(10, 0, -1):
+    print(c)
+    time.sleep(1)
+
