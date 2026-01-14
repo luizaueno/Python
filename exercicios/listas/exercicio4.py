7f08e10cd526fafb4c4b8e1abe8f327f070f8877
luiza <161471348+luizaueno@users.noreply.github.com>
2026-01-14 20:21:30 -0300

    resolvido erro do exercicio4

--- exercicios/listas/exercicio4.py
+++ exercicios/listas/exercicio4.py
@@ -21,7 +21,7 @@ print(galera)
 print('-' * 70)
 print(f'As idades são: {galera[0][1]}, {galera[1][1]}, {galera[2][1]}')
 print('-' * 70)
-m = int(galera[0][1]) + int(galera[1][1]) + int(galera[2][1])/3
+m = ((galera[0][1]) + (galera[1][1]) + (galera[2][1]))/3
 print(f'A média das idades é {m}')
 print('-' * 70)
 print('Todas são maiores de idade')
