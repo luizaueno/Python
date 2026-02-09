55e19ebb0590967863117bf6fc3ec9b4e2debcc5
luiza <161471348+luizaueno@users.noreply.github.com>
2026-02-09 15:10:12 -0300

    desafio 001 corrigido python POO

--- poo/desafios/001/001.py
+++ poo/desafios/001/001.py
@@ -2,13 +2,18 @@
 
 # Declaração de classe
 class Funcionario:
+    # Atributos de classe - serve para todos os objetos da classe
+    empresa = "L.A Tech"
+
     def __init__(self, nome, cargo, setor):
+
+        # Atributos de instancia - específicos do objeto, únicos
         self.nome = nome
         self.cargo = cargo
         self.setor = setor
         
-    def apresentacao(self):
-        return f"Olá, sou {self.nome} e sou {self.cargo} do setor de {self.setor} da empresa LA tech"
+    def apresentacao(self) -> str:
+        return f"Olá, sou {self.nome} e sou {self.cargo} do setor de {self.setor} da empresa {Funcionario.empresa} "
     
 # Declaração de objetos 
 f1 = Funcionario("Luiza", "Engenheira de software embarcados", "TI")
